e1c6c85f45fce219750a7b1a9bca859858b1228d
wassimamdouni <wassim.amdouni28@gmail.com>
2023-11-25 14:33:53 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,2 @@
-# py get-pip.py
-# pip install django 
-# pip install google
-# python.exe -m pip install --upgrate pip
-# pip install --upgrate google-api-python-client
-# pip install djangorestframework
-# pip install django-cors-headers
-# pip install django-phonenumber-field
-# pip install phonenumbers
-# pip install firebase-admin
-# python -m pip install Pillow
-# pip install django-cors-headers
-# pip install django-storeges
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
