b69a369605144c5153d287ec32e0953f730cedbf
wassimamdouni <wassim.amdouni28@gmail.com>
2023-11-26 13:41:44 +0100

    Update

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,2 @@
-pip install psycopg2-binary
-pip install -r requirements.txt 
+pip3.9 install --disable-pip-version-check --target . --upgrade -r /vercel/path0/requirements.txt
 python3.9 manage.py collectstatic
